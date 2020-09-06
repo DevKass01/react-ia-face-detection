17ca53f483de0df5473362109e23efb3185b55fe
aKass <aKass@users.noreply.github.com>
2020-09-06 14:58:17 +0200

    Updates

---
+++ precache-manifest.a3185b12e02211d6abac25ac43f056f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a67e95c94e2645f1ac938434c59d51bf",
+    "url": "/react-ia-face-detection/index.html"
+  },
+  {
+    "revision": "3764888526356a7d99a9",
+    "url": "/react-ia-face-detection/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "654c04ef7a06194efaa2",
+    "url": "/react-ia-face-detection/static/css/main.2c81b90f.chunk.css"
+  },
+  {
+    "revision": "3764888526356a7d99a9",
+    "url": "/react-ia-face-detection/static/js/2.4a33c381.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/react-ia-face-detection/static/js/2.4a33c381.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "654c04ef7a06194efaa2",
+    "url": "/react-ia-face-detection/static/js/main.b441b187.chunk.js"
+  },
+  {
+    "revision": "8ba014f99659f4a072b5",
+    "url": "/react-ia-face-detection/static/js/runtime-main.904a50ce.js"
+  }
+]);
